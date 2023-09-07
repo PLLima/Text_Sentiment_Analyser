3aee87b0cf28de0e4eebd4af78e9f796e9d4534f
PLLima <{ID}+{username}@users.noreply.github.com>
2023-09-07 20:00:22 -0300

    Added input arguments' validation

--- sll_app/src/main.c
+++ sll_app/src/main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "input_output.h"
 #include "sll.h"
 
 int main(int argc, char *argv[])
 {
-    sll_comp_find = 0;                      // Reset comparison counters
+    validation_t input_args = Invalid;              // Initialize local variables
+
+    sll_comp_find = 0;                              // Reset comparison counters
     sll_comp_insert = 0;
     
+    input_args = validate_input_arguments(argc);    // Validate input arguments
+    if(input_args == Invalid)
+        return input_args;
+    
+    
+
     return 0;
 }
